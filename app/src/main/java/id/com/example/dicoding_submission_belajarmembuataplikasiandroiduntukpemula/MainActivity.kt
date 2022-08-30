7c80dc00b798ceb807e4f324ce5b9e13f86769d2
kiohio707 <fernata707@gmail.com>
2022-08-30 19:40:19 +0700

    Initial Commit

---
+++ app/src/main/java/id/com/example/dicoding_submission_belajarmembuataplikasiandroiduntukpemula/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.com.example.dicoding_submission_belajarmembuataplikasiandroiduntukpemula
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
